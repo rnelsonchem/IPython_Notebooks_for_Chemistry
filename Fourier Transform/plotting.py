241226af58615017b26317b47a9bf49b09ee5609
Ryan Nelson <rnelsonchem@gmail.com>
2013-03-29 11:52:28 -0400

    Removed plotting functions, import instead
    Went through all of the notebooks, and removed the explicit definition
    of the plotting functions. Instead, I do a %run on the plotting.py file
    to import these functions from a global location. Made a couple of small
    text modifications, and changed the colors of the FT plots in
    plotting.py.

--- Fourier Transform/plotting.py
+++ Fourier Transform/plotting.py
@@ -13,11 +13,11 @@ def dual_plot_time(xdata, ydata, xmin=None, xmax=None):
 def dual_plot_ft(xdata, ydata, sort_idx, xmin=None, xmax=None):
     plt.figure(figsize=(12,4))
     ax1 = plt.subplot(121)
-    plt.plot(xdata[sort_idx], ydata.real[sort_idx], 'g')
+    plt.plot(xdata[sort_idx], ydata.real[sort_idx])
     plt.title('Real')
 
     plt.subplot(122, sharey=ax1, sharex=ax1)
-    plt.plot(xdata[sort_idx], ydata.imag[sort_idx], 'g')
+    plt.plot(xdata[sort_idx], ydata.imag[sort_idx])
     plt.title('Imaginary')
     plt.xlim(xmin, xmax)
 
